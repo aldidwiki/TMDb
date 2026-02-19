ac96a03939fe74fad6443c17ad1824135b1650c0
Aldi Prahasta <aldidwiki@gmail.com>
2026-02-19 13:30:04 +0700

    [chore] Enhance ExpandableTextView

--- TMDb/Core/CustomView/ExpandableTextView.swift
+++ TMDb/Core/CustomView/ExpandableTextView.swift
@@ -9,30 +9,32 @@ import SwiftUI
 
 struct ExpandableTextView: View {
     var textData: String
-    var maxTextLength = 70
+    var maxTextLine = 3
     
     @State var isExpanded = false
     
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 10) {
             Text(textData)
                 .multilineTextAlignment(.leading)
-                .modifier(AnimatingCellHeight(height: isExpanded ? CGFloat(Double(textData.count)/1.3) : CGFloat(maxTextLength)))
+                .lineLimit(isExpanded ? nil : maxTextLine)
+                .transition(.opacity)
             
-            if textData.count > maxTextLength {
-                Button {
-                    withAnimation {
-                        isExpanded.toggle()
-                    }
-                } label: {
-                    Label(isExpanded ? "Collapse" : "Expand", systemImage: isExpanded ? "chevron.up" : "chevron.down")
-                        .font(.system(size: 14))
-                        .fontWeight(.medium)
-                        .labelStyle(CustomLabelStyle(spacing: 2))
+            Button {
+                withAnimation(.spring(response: 0.45, dampingFraction: 0.85, blendDuration: 0)) {
+                    isExpanded.toggle()
                 }
-                .buttonStyle(.borderless)
-                .frame(maxWidth: .infinity, alignment: .center)
+            } label: {
+                HStack(spacing: 4) {
+                    Text(isExpanded ? "Collapse" : "Expand")
+                    Image(systemName: "chevron.up")
+                        .rotationEffect(.degrees(isExpanded ? 0 : 180))
+                }
+                .font(.system(size: 14))
+                .fontWeight(.medium)
             }
+            .buttonStyle(.borderless)
+            .frame(maxWidth: .infinity, alignment: .center)
         }
     }
 }
